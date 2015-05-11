f33adc4b541f7bc8cf444b6e46fafedefd4cb2f1
Jim Yin <xiangpiduzi@gmail.com>
2015-05-12 00:33:30 +0800

    img upload ok!

--- src/main/java/rental/web/RoomController.java
+++ src/main/java/rental/web/RoomController.java
@@ -1,13 +1,8 @@
 package rental.web;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import rental.domain.Account;
 import rental.domain.Room;
@@ -26,9 +20,6 @@ import javax.validation.Valid;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -62,7 +53,7 @@ public class RoomController {
     public String showRoom(@PathVariable Long id, Model model) {
         Room room = roomRepository.findOne(id);
         model.addAttribute("room", room);
-        logger.info(":::Show room detail, roomID="+id);
+        logger.info(":::Show room detail, roomID=" + id);
         return "room_detail";
     }
 
@@ -73,12 +64,13 @@ public class RoomController {
         return "room_list";
     }
 
-    @RequestMapping(method = RequestMethod.POST, consumes = "multipart/form-data")
+    @RequestMapping(method = RequestMethod.POST)
     @Secured("ROLE_USER")
-    public String postRoom(@Valid @ModelAttribute Room room, BindingResult result,
-                           Model model, @RequestParam("file") MultipartFile[] files) {
+    public String postRoom(@Valid @ModelAttribute Room room, BindingResult result, Model model,
+                           @RequestParam("photoFiles[]") MultipartFile[] files) {
         if (result.hasErrors()) {
-            return "room_form";
+            model.addAttribute(room);
+            return "redirect:/rooms/form";
         }
 
         String username = ((UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
@@ -88,21 +80,21 @@ public class RoomController {
 
         List<String> photoUris = new ArrayList<>(5);
 
-        for (int i = 0; i < files.length; i++) {
-            File photoFile = new File(userPhotoPath, files[i].getOriginalFilename());
-            if (!files[i].isEmpty()) {
+        for (MultipartFile file : files) {
+            File photoFile = new File(userPhotoPath, file.getOriginalFilename());
+            if (!file.isEmpty()) {
                 try {
-                    byte[] bytes = files[i].getBytes();
+                    byte[] bytes = file.getBytes();
                     BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(photoFile));
                     stream.write(bytes);
                     stream.close();
-                    photoUris.add("/photos/" + username + "/" + files[i].getOriginalFilename());
+                    photoUris.add("/photos/" + username + "/" + file.getOriginalFilename());
                     logger.info("::: Post room, uploaded " + photoFile + "!");
                 } catch (Exception e) {
                     logger.info("::: Post room upload error: " + photoFile + " => " + e.getMessage());
                 }
             } else {
-                logger.info("::: Post room upload error: file " + files[i].getOriginalFilename() + " was empty.");
+                logger.info("::: Post room upload error: file " + file.getOriginalFilename() + " was empty.");
             }
         }
 
@@ -112,49 +104,9 @@ public class RoomController {
         logger.info(":::Post room, user=" + username);
         room.setAccountId(account.getId());
 
-//        resolveGeocoding(room);
-
         room.setLastModified(new Date());
         roomRepository.save(room);
-        return "room_list";
-    }
-
-    private void resolveGeocoding(Room room) {
-        String url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + room.getAddress() + "&sensor=false";
-        ResponseEntity<String> entity;
-        try {
-            entity = new RestTemplate().getForEntity(url, String.class);
-        } catch (Exception e) {
-            logger.info(":::Get Google api failed, retry with proxy, exption: " + e);
-            SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-            Proxy proxy= new Proxy(Proxy.Type.HTTP, new InetSocketAddress("3.87.248.1", 88));
-            requestFactory.setProxy(proxy);
-            entity = new RestTemplate(requestFactory).getForEntity(url, String.class);
-        }
-        HttpStatus status = entity.getStatusCode();
-        if (status.is2xxSuccessful()) {
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode jsonNode;
-            try {
-                jsonNode = mapper.readTree(entity.getBody());
-            } catch (IOException e) {
-                logger.info(":::Parse json failed, exption: " + e);
-                return;
-            }
-            String googleStatus = jsonNode.get("status").asText();
-            if ("OK".equals(googleStatus)) {
-                JsonNode latlng = jsonNode.get("results").get(0).get("geometry").get("location");
-                double lat = latlng.get("lat").asDouble();
-                room.setLat(lat);
-                double lng = latlng.get("lng").asDouble();
-                room.setLat(lng);
-                logger.info(":::Got Geocoding from Google, Lat: " + lat + ", Lng: " + lng);
-            } else {
-                logger.error(":::Google Geocoding return status other than OK, the status is: " + googleStatus);
-            }
-        } else {
-            logger.error(":::Error while parse address to geocoding, the status is: " + status);
-        }
+        return "redirect:/rooms";
     }
 
     @RequestMapping(value = "/{room}", method = RequestMethod.PUT)
