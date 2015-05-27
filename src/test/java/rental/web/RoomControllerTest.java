4b39757e6cef7f47d4e38c00737fcaea90490ec2
Jim Yin <xiangpiduzi@gmail.com>
2015-05-27 22:56:08 +0800

    update unit test

--- src/test/java/rental/web/RoomControllerTest.java
+++ src/test/java/rental/web/RoomControllerTest.java
@@ -1,15 +1,11 @@
 package rental.web;
 
-import org.junit.Test;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import rental.Main;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Main.class)
 public class RoomControllerTest {
-    @Test
-    public void testListRooms() {
-    }
 }
